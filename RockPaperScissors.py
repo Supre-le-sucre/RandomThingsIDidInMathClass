488fcafa3f5fec4388c4994151d14dba8840143a
Supre-le-sucre <59318031+Supre-le-sucre@users.noreply.github.com>
2021-12-14 18:14:42 +0100

    Added exception handler
    while choice is not a valid int, program won't go further

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,11 +3,23 @@ round = 1
 string = ""
 player_point = 0
 cpu_point = 0
+choice = 0
+Unplayable = True
 c = "y"
 while(c == "y"):
     print("===========")
     if(cpu_point < 3 and player_point < 3):
-        choice = int(input("Round %s \n1- Rock \n2- Paper\n3- Scissors\n" % round))
+        Unplayable = True
+        while(Unplayable):
+            choice = input("Round %s \n1- Rock \n2- Paper\n3- Scissors\n" % round)
+            if not choice.isnumeric():
+                print("Please provide a valid option !")
+            else:
+                choice = int(choice)
+                if(choice>3 or choice<=0):
+                    print("Please provide a valid option !")
+                else:
+                    Unplayable = False
         if(choice == 1): string = "rock"
         if(choice == 2): string = "paper"
         if(choice == 3): string = "scissors"

0b985cf3e78d30abcf57f795ed71e6679fd52e89
Supre <59318031+Supre-le-sucre@users.noreply.github.com>
2022-02-16 15:19:30 +0100

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,10 +1,10 @@
 def prime(n):
     tab = [2]
     # Show all prime numbers until n
-    for number in range(2, n+1):
+    for number in range(3, n + 1):
         for i in range(len(tab)):
             if number % tab[i] == 0:
                 break
-        if i == len(tab)-1 and number != 2:
+        if i == len(tab) - 1:
             tab.append(number)
     return tab
